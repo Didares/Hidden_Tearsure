c0b4b7602964881b47abf54733aab5d8d84396ca
Wicko-Design <david@wicko.co.uk>
2016-02-01 17:59:55 +0000

    Initial Commit

---
+++ MapSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapSearch
+//
+//  Created by iMac 27" on 01/02/2016.
+//  Copyright © 2016 Wicko Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
